f718d18c0083dfd2470b9c6e169ba06494676d24
Matteo Golin <matteo.golin@gmail.com>
2023-08-14 09:39:29 -0400

    added three new commands: StandEvery, StandEnable, StandDisable.

--- lua/stand.lua
+++ lua/stand.lua
@@ -5,6 +5,7 @@ function M.setup(options)
 	options.minute_interval = (options.minute_interval or 60) * 60 * 1000
 
 	local timer = vim.loop.new_timer()
+	local enabled = true
 
 	local has_notify, notify = pcall(require, "notify")
 	if has_notify then
@@ -35,7 +36,7 @@ function M.setup(options)
 				minutes_remaining == 1 and "minute" or "minutes"
 			)
 		)
-	end, {})
+	end, { desc = "Display time remaining until next stand." })
 
 	vim.api.nvim_create_user_command("StandNow", function()
 		if has_notify then
@@ -44,7 +45,37 @@ function M.setup(options)
 
 		timer:stop()
 		timer:again()
-	end, {})
+	end, { "Stand, dismissing any notifications and restarting the timer." })
+
+	vim.api.nvim_create_user_command("StandEvery", function(opts)
+		options.minute_interval = tonumber(opts.args) * 60 * 1000
+		local remaining = timer:get_due_in()
+		timer:stop()
+		timer:start(remaining, options.minute_interval, vim.schedule_wrap(on_timer)) -- Start timer with new time
+	end, { desc = "Set the stand interval in minutes for future timers.", nargs = 1 })
+
+	vim.api.nvim_create_user_command("StandDisable", function()
+		if not enabled then
+			vim.print("Standing already disabled.")
+			return
+		end
+		timer:stop()
+		vim.print("Standing disabled.")
+		enabled = false
+		if has_notify then
+			require("notify").dismiss()
+		end
+	end, { desc = "Disable the stand timer." })
+
+	vim.api.nvim_create_user_command("StandEnable", function()
+		if enabled then
+			vim.print("Standing already enabled.")
+			return
+		end
+		timer:again()
+		vim.print("Standing enabled.")
+		enabled = true
+	end, { desc = "Enable the stand timer." })
 end
 
 return M
